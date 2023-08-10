d1fd7157961a25da6cd52bf990a99c32bafee85a
julieprunaret <j.prunaret@gmail.com>
2023-08-10 14:39:09 +0200

    init mouseEffect exercice over

--- exercices/mouseeffect/index.js
+++ exercices/mouseeffect/index.js
@@ -5,3 +5,15 @@
 // S'assurer que les liens sont clickables
 
 // Donner un style de transparence aux 2 plus gros ronds (mix-blend-mode)
+const circleMaker = (size) => {
+    let circle = document.createElement("div");
+    circle.classList.add("circle",`${size}`);
+    document.body.appendChild(circle);
+    window.addEventListener('mousemove', (e) => {
+        circle.style.setProperty("--left", e.pageX + "px");
+        circle.style.setProperty("--y", e.pageY + "px");
+    })
+
+}
+
+["smallCircle","mediumCircle","largeCircle"].forEach(circleMaker);
